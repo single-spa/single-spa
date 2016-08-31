ccefb4c9e5e48ea6b5af57259b36e5beadcb73d1
Joel Denning <joeldenning@gmail.com>
2016-08-30 23:49:02 -0600

    spaces to tabs

--- src/child-applications/child-apps.js
+++ src/child-applications/child-apps.js
@@ -16,31 +16,31 @@ export function getAppStatus(appName) {
 }
 
 export function declareChildApplication(appLocation, arg1, arg2) {
-    if (typeof appLocation !== 'string' || appLocation.length === 0)
-        throw new Error(`The first argument must be a non-empty string 'appLocation'`);
-    if (childApps[appLocation])
-        throw new Error(`There is already an app declared at location ${appLocation}`);
+	if (typeof appLocation !== 'string' || appLocation.length === 0)
+		throw new Error(`The first argument must be a non-empty string 'appLocation'`);
+	if (childApps[appLocation])
+		throw new Error(`There is already an app declared at location ${appLocation}`);
 
 	let loadImpl, activeWhen;
 	if (!arg2) {
 		if (!Loader) {
 			throw new Error(`You cannot declare a single-spa child application without either providing a way to load the application or a Loader. See https://github.com/CanopyTax/single-spa/blob/master/docs/single-spa-api.md#declarechildapplication`);
 		}
-		loadImpl = () => Loader.import(appLocation);
-		activeWhen = arg1;
+			loadImpl = () => Loader.import(appLocation);
+			activeWhen = arg1;
 	} else {
 		loadImpl = arg1;
 		activeWhen = arg2;
 	}
-    if (typeof activeWhen !== 'function')
-        throw new Error(`The second argument must be a function 'activeWhen'`);
+	if (typeof activeWhen !== 'function')
+		throw new Error(`The second argument must be a function 'activeWhen'`);
 
-    childApps.push({
-        appLocation,
+	childApps.push({
+		appLocation,
 		loadImpl,
-        activeWhen,
+		activeWhen,
 		status: NOT_LOADED,
-    });
+	});
 
 	ensureJQuerySupport();
 
