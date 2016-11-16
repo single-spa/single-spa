304adaa6247161166885793d59a33b0eaec6da85
Joel Denning <joeldenning@gmail.com>
2016-11-16 14:00:00 -0700

    Switching to webpack to make UMD build. Fixes #79. (#85)
    * Switching to webpack to make UMD build. Fixes #79.
    
    * Self review and fixing tests

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		__dirname + '/src/single-spa.js',
+	],
+	output: {
+		path: __dirname + '/lib',
+		filename: 'single-spa.js',
+		library: 'singleSpa',
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+	},
+	devtool: 'source-map',
+	resolve: {
+		modules: [
+			"node_modules",
+			path.resolve(__dirname),
+		],
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+			},
+		],
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: true,
+			test: /\.js($|\?)/i,
+		}),
+	],
+}
