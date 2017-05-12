9c82ea6778f7f4ba50f1e777f436a14e036d9262
Joel Denning <joeldenning@gmail.com>
2017-05-12 11:11:10 -0600

    Implementing #98. First mount events (#101)
    * Implementing #98. First mount events
    
    * Fixing IE11
    
    * Docs
    
    * Fix docs
    
    * More docs

--- src/child-applications/lifecycles/mount.js
+++ src/child-applications/lifecycles/mount.js
@@ -1,17 +1,32 @@
 import { NOT_MOUNTED, MOUNTED, SKIP_BECAUSE_BROKEN } from '../child-app.helpers.js';
 import { handleChildAppError } from '../child-app-errors.js';
 import { reasonableTime } from '../timeouts.js';
+import CustomEvent from 'custom-event';
+
+let beforeFirstMountFired = false;
+let firstMountFired = false;
 
 export async function toMountPromise(app) {
 	if (app.status !== NOT_MOUNTED) {
 		return app;
 	}
+
+	if (!beforeFirstMountFired) {
+		window.dispatchEvent(new CustomEvent('single-spa:before-first-mount'));
+		beforeFirstMountFired = true;
+	}
+
 	try {
 		await reasonableTime(app.mount({childAppName: app.name}), `Mounting application '${app.name}'`, app.timeouts.mount);
 		app.status = MOUNTED;
 	} catch (err) {
 		handleChildAppError(err, app);
 		app.status = SKIP_BECAUSE_BROKEN;
+	}
+
+	if (!firstMountFired) {
+		window.dispatchEvent(new CustomEvent('single-spa:first-mount'));
+		firstMountFired = true;
 	}
 
 	return app;
