43d21945d9fab1d4bafa241ed8778fa31cd0a6db
Justin McMurdie <mcmurdiej@gmail.com>
2018-01-05 11:57:07 -0700

    Spaces

--- spec/apps/register-with-object/register-with-object.spec.js
+++ spec/apps/register-with-object/register-with-object.spec.js
@@ -16,50 +16,50 @@ const app = {
 };
 
 export default function() {
-	describe(`register-with-object`, () => {
-		beforeAll(() => {
+  describe(`register-with-object`, () => {
+    beforeAll(() => {
       singleSpa.registerApplication(
         'register-with-object',
         app,
         location => location.hash === "#register-with-object"
       );
-		});
+    });
 
     beforeEach(() => {
-			location.hash = '#not-register-with-object';
+      location.hash = '#not-register-with-object';
 
       bootstrapped = false;
       mounted = false;
-		})
+    })
 
-		it(`goes through the whole lifecycle successfully`, (done) => {
-			expect(mounted).toEqual(false);
-			expect(singleSpa.getMountedApps()).toEqual([]);
+    it(`goes through the whole lifecycle successfully`, (done) => {
+      expect(mounted).toEqual(false);
+      expect(singleSpa.getMountedApps()).toEqual([]);
 
       location.hash = '#register-with-object';
 
-			singleSpa
-			.triggerAppChange()
-			.then(() => {
-				expect(bootstrapped).toEqual(true);
-				expect(mounted).toEqual(true);
-				expect(singleSpa.getMountedApps()).toEqual(['register-with-object']);
+      singleSpa
+        .triggerAppChange()
+        .then(() => {
+          expect(bootstrapped).toEqual(true);
+          expect(mounted).toEqual(true);
+          expect(singleSpa.getMountedApps()).toEqual(['register-with-object']);
 
-				location.hash = '#not-register-with-object';
+          location.hash = '#not-register-with-object';
 
-				return singleSpa
-          .triggerAppChange()
-          .then(() => {
-            expect(bootstrapped).toEqual(true);
-            expect(mounted).toEqual(false);
-            expect(singleSpa.getMountedApps()).toEqual([]);
-            done();
-          })
-			})
-			.catch(ex => {
-				fail(ex);
-				done();
-			})
-		});
-	});
+          return singleSpa
+            .triggerAppChange()
+            .then(() => {
+              expect(bootstrapped).toEqual(true);
+              expect(mounted).toEqual(false);
+              expect(singleSpa.getMountedApps()).toEqual([]);
+              done();
+            })
+        })
+        .catch(ex => {
+          fail(ex);
+          done();
+        })
+    });
+  });
 }
