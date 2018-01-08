d1fa2cfa54de83d7603d4c5340d616f0c32215e1
Justin McMurdie <mcmurdiej@gmail.com>
2018-01-08 15:38:51 -0700

    tabs to spaces in additional files I missed (#146)

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,35 @@ var webpack = require('webpack');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: [
-		__dirname + '/src/single-spa.js',
-	],
-	output: {
-		path: __dirname + '/lib',
-		filename: 'single-spa.js',
-		library: 'singleSpa',
-		libraryTarget: 'umd',
-		umdNamedDefine: true,
-	},
-	devtool: 'source-map',
-	resolve: {
-		modules: [
-			"node_modules",
-			path.resolve(__dirname),
-		],
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-loader',
-			},
-		],
-	},
-	plugins: [
-		new CleanWebpackPlugin(['lib'], {
-			verbose: true,
-		})
-	],
+  entry: [
+    __dirname + '/src/single-spa.js',
+  ],
+  output: {
+    path: __dirname + '/lib',
+    filename: 'single-spa.js',
+    library: 'singleSpa',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  devtool: 'source-map',
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname),
+    ],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['lib'], {
+      verbose: true,
+    })
+  ],
 }
