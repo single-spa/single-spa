138f7885b18b4bd8aac1771129a6e36d9591f107
Justin McMurdie <mcmurdiej@gmail.com>
2018-01-18 15:55:11 -0700

    Changing bootstrap to use AppOrParcel

--- src/applications/lifecycles/bootstrap.js
+++ src/applications/lifecycles/bootstrap.js
@@ -3,20 +3,20 @@ import { reasonableTime } from '../timeouts.js';
 import { handleAppError } from '../app-errors.js';
 import { getProps } from './prop.helpers.js'
 
-export async function toBootstrapPromise(app) {
-  if (app.status !== NOT_BOOTSTRAPPED) {
-    return app;
+export async function toBootstrapPromise(appOrParcel) {
+  if (appOrParcel.status !== NOT_BOOTSTRAPPED) {
+    return appOrParcel;
   }
 
-  app.status = BOOTSTRAPPING;
+  appOrParcel.status = BOOTSTRAPPING;
 
   try {
-    await reasonableTime(app.bootstrap(getProps(app)), `Bootstrapping app '${app.name}'`, app.timeouts.bootstrap);
-    app.status = NOT_MOUNTED;
+    await reasonableTime(appOrParcel.bootstrap(getProps(appOrParcel)), `Bootstrapping appOrParcel '${appOrParcel.name}'`, appOrParcel.timeouts.bootstrap);
+    appOrParcel.status = NOT_MOUNTED;
   } catch(err) {
-    app.status = SKIP_BECAUSE_BROKEN;
-    handleAppError(err, app);
+    appOrParcel.status = SKIP_BECAUSE_BROKEN;
+    handleAppError(err, appOrParcel);
   }
 
-  return app;
+  return appOrParcel;
 }
