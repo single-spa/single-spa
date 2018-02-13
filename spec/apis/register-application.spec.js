7ba9adccdf094be1d154d76241006aec9dba9794
Joel Denning <joel.denning@canopytax.com>
2018-02-12 21:34:06 -0700

    Updating api tests

--- spec/apis/register-application.spec.js
+++ spec/apis/register-application.spec.js
@@ -1,27 +1,27 @@
-export default function() {
-  describe('registerApplication', function() {
-    let app
-    beforeEach(() => {
-      app = {
-        mount(){
-          return Promise.resolve()
-        },
-        unmount() {
-          return Promise.resolve()
-        },
-        bootstrap() {
-          return Promise.resolve()
-        }
-      }
-    })
+import * as singleSpa from 'single-spa';
 
-    it('should throw when I register the same app twice', () => {
-      singleSpa.registerApplication('duplicateApp', app, () => true)
-      expect(
-        () => {
-          singleSpa.registerApplication('duplicateApp', app, () => true)
-        }
-      ).toThrow()
-    })
+describe('registerApplication', function() {
+  let app
+  beforeEach(() => {
+    app = {
+      mount(){
+        return Promise.resolve()
+      },
+      unmount() {
+        return Promise.resolve()
+      },
+      bootstrap() {
+        return Promise.resolve()
+      }
+    }
   })
-}
+
+  it('should throw when I register the same app twice', () => {
+    singleSpa.registerApplication('duplicateApp', app, () => true)
+    expect(
+      () => {
+        singleSpa.registerApplication('duplicateApp', app, () => true)
+      }
+    ).toThrow()
+  })
+})
