9b94fd8cd73c70684ae41f933ba4437c4641ae76
Anthony Frehner <anthony.frehner@canopytax.com>
2019-01-29 12:43:28 -0700

    self review -- remove ununsed import and cleanup getAppData

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -1,8 +1,5 @@
 import {getRawAppData} from '../applications/apps'
-import {removeCyclicReferences} from './devtools.helper'
 
 export function getAppData() {
-  const apps = getRawAppData()
-
-  return removeCyclicReferences(apps)
+  return getRawAppData()
 }
