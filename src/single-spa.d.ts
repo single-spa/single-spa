41562391297d10d82fe814dd563e9980bc4145bc
Alessandro De Blasis <alex@deblasis.net>
2019-04-21 10:59:20 +0100

    chore(typings): added typings
    Added typings by looking at the API/src

---
+++ src/single-spa.d.ts
@@ -0,0 +1,99 @@
+declare module "single-spa" {
+  // ./start.js
+  export function start(): void;
+  export function isStarted(): boolean;
+
+  // ./jquery-support.js
+  export function ensureJQuerySupport(jQuery?: Object): void;
+
+  // ./applications/timeouts.js
+  export function setBootstrapMaxTime(
+    time: number,
+    dieOnTimeout?: boolean
+  ): void;
+  export function setMountMaxTime(time: number, dieOnTimeout?: boolean): void;
+  export function setUnmountMaxTime(time: number, dieOnTimeout?: boolean): void;
+  export function setUnloadMaxTime(time: number, dieOnTimeout?: boolean): void;
+
+  type timeoutConfig = {
+    millis: number;
+    dieOnTimeout: boolean;
+  };
+
+  type globalTimeoutConfig = {
+    bootstrap: timeoutConfig;
+    mount: timeoutConfig;
+    unmount: timeoutConfig;
+    unload: timeoutConfig;
+  };
+
+  export function reasonableTime(
+    promise: Promise<any>,
+    description: string,
+    timeoutConfig: timeoutConfig
+  ): void;
+
+  export function ensureValidAppTimeouts(
+    timeouts?: globalTimeoutConfig
+  ): globalTimeoutConfig;
+
+  // ./applications/apps.js
+  export function registerApplication(
+    appName: string,
+    applicationOrLoadingFn: Function | Promise<any>,
+    activityFn: (location: Location) => boolean,
+    customProps?: Object
+  ): void;
+
+  export function getMountedApps(): string[];
+
+  declare enum AppStatus {
+    NOT_LOADED = "NOT_LOADED",
+    LOADING_SOURCE_CODE = "LOADING_SOURCE_CODE",
+    NOT_BOOTSTRAPPED = "NOT_BOOTSTRAPPED",
+    BOOTSTRAPPING = "BOOTSTRAPPING",
+    NOT_MOUNTED = "NOT_MOUNTED",
+    MOUNTING = "MOUNTING",
+    MOUNTED = "MOUNTED",
+    UPDATING = "UPDATING",
+    UNMOUNTING = "UNMOUNTING",
+    UNLOADING = "UNLOADING",
+    SKIP_BECAUSE_BROKEN = "SKIP_BECAUSE_BROKEN"
+  }
+
+  export function getAppStatus(appName: string): AppStatus | null;
+
+  export function unloadApplication(
+    appName: string,
+    opts?: { waitForUnmount: boolean }
+  ): Promise<any>;
+
+  export function checkActivityFunctions(location: Location): string[];
+  export function getAppNames(): string[];
+
+  // ./navigation/navigation-events.js'
+  export function navigateToUrl(
+    obj:
+      | string
+      | {
+          currentTarget: {
+            href: string;
+          };
+          preventDefault: any;
+        },
+    opts?: Object
+  ): void;
+
+  // './navigation/reroute.js'
+  export function triggerAppChange(
+    pendingPromises?: any[],
+    eventArguments?: any
+  ): Promise<any>;
+
+  // './applications/app-errors.js'
+  export function addErrorHandler(handler: (error: Error) => void): void;
+  export function removeErrorHandler(handler: (error: Error) => void): void;
+
+  // './parcels/mount-parcel.js'
+  export function mountRootParcel(parcelConfig: any, parcelProps: any): boolean;
+}
