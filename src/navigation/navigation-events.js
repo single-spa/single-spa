edd93e664989b10ad45d5c51d766ba2e3f059f47
Joel Denning <joeldenning@gmail.com>
2019-12-01 17:01:20 +0200

    Refactoring navigateToUrl to reduce byte size

--- src/navigation/navigation-events.js
+++ src/navigation/navigation-events.js
@@ -13,7 +13,7 @@ const capturedEventListeners = {
 
 export const routingEventsListeningTo = ["hashchange", "popstate"];
 
-export function navigateToUrl(obj, opts = {}) {
+export function navigateToUrl(obj) {
   let url;
   if (typeof obj === "string") {
     url = obj;
@@ -37,28 +37,21 @@ export function navigateToUrl(obj, opts = {}) {
   const destination = parseUri(url);
 
   if (url.indexOf("#") === 0) {
-    window.location.hash = "#" + destination.anchor;
+    window.location.hash = destination.hash;
   } else if (current.host !== destination.host && destination.host) {
-    if (opts.isTestingEnv) {
+    if (process.env.BABEL_ENV === "test") {
       return { wouldHaveReloadedThePage: true };
     } else {
       window.location.href = url;
     }
   } else if (
-    !isSamePath(
-      destination.path + "?" + destination.query,
-      current.path + "?" + current.query
-    )
+    destination.pathname === current.pathname &&
+    destination.search === current.pathname
   ) {
+    window.location.hash = destination.hash;
+  } else {
     // different path, host, or query params
     window.history.pushState(null, null, url);
-  } else {
-    window.location.hash = "#" + destination.anchor;
-  }
-
-  function isSamePath(destination, current) {
-    // if the destination has a path but no domain, it doesn't include the root '/'
-    return current === destination || current === "/" + destination;
   }
 }
 
@@ -148,49 +141,7 @@ function createPopStateEvent(state) {
 window.singleSpaNavigate = navigateToUrl;
 
 function parseUri(str) {
-  // parseUri 1.2.2
-  // (c) Steven Levithan <stevenlevithan.com>
-  // MIT License
-  // http://blog.stevenlevithan.com/archives/parseuri
-  const parseOptions = {
-    strictMode: true,
-    key: [
-      "source",
-      "protocol",
-      "authority",
-      "userInfo",
-      "user",
-      "password",
-      "host",
-      "port",
-      "relative",
-      "path",
-      "directory",
-      "file",
-      "query",
-      "anchor"
-    ],
-    q: {
-      name: "queryKey",
-      parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-    },
-    parser: {
-      strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-      loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
-    }
-  };
-
-  let o = parseOptions;
-  let m = o.parser[o.strictMode ? "strict" : "loose"].exec(str);
-  let uri = {};
-  let i = 14;
-
-  while (i--) uri[o.key[i]] = m[i] || "";
-
-  uri[o.q.name] = {};
-  uri[o.key[12]].replace(o.q.parser, function($0, $1, $2) {
-    if ($1) uri[o.q.name][$1] = $2;
-  });
-
-  return uri;
+  const anchor = document.createElement("a");
+  anchor.href = str;
+  return anchor;
 }
