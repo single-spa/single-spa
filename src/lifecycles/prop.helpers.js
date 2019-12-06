43fa2e1bc8d0abcae43fd73af37ca0eda34a212d
Joel Denning <joeldenning@gmail.com>
2019-12-06 06:35:16 +0200

    Switching from object rest spread to manual assign.

--- src/lifecycles/prop.helpers.js
+++ src/lifecycles/prop.helpers.js
@@ -1,13 +1,13 @@
 import * as singleSpa from "../single-spa.js";
 import { mountParcel } from "../parcels/mount-parcel.js";
+import { assign } from "../utils/assign.js";
 
 export function getProps(appOrParcel) {
-  const result = {
-    ...appOrParcel.customProps,
+  const result = assign({}, appOrParcel.customProps, {
     name: appOrParcel.name,
     mountParcel: mountParcel.bind(appOrParcel),
     singleSpa
-  };
+  });
 
   if (appOrParcel.unmountThisParcel) {
     result.unmountSelf = appOrParcel.unmountThisParcel;
