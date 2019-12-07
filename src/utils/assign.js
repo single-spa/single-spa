dae28ab585c4cd6454d0de17a78ae7175cbfb227
Joel Denning <joeldenning@gmail.com>
2019-12-07 07:05:08 +0200

    Prototype pollution

--- src/utils/assign.js
+++ src/utils/assign.js
@@ -3,6 +3,9 @@
 export function assign() {
   for (let i = arguments.length - 1; i > 0; i--) {
     for (let key in arguments[i]) {
+      if (key === "__proto__") {
+        continue;
+      }
       arguments[i - 1][key] = arguments[i][key];
     }
   }
