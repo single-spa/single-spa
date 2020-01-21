a3b56550227deac1023663c54b07d0b0ae2bf9f6
Tanner Lyons <tanner.m.lyons@gmail.com>
2020-01-21 10:19:47 -0800

    add extra properties to error objects
    From [the docs](https://single-spa.js.org/docs/api#adderrorhandler):
    errorHandler: Function(error: Error)
    Must be a function. Will be called with an Error object that additionally has a message and appOrParcelName property
    
    This commit adds these extra props to the ts typings.

--- typings/single-spa.d.ts
+++ typings/single-spa.d.ts
@@ -105,8 +105,13 @@ declare module "single-spa" {
   export function triggerAppChange(): Promise<any>;
 
   // './applications/app-errors.js'
-  export function addErrorHandler(handler: (error: Error) => void): void;
-  export function removeErrorHandler(handler: (error: Error) => void): void;
+  type AppError = Error & {
+    appOrParcelName: string;
+    message: string;
+  }
+  export function addErrorHandler(handler: (error: AppError) => void): void;
+  export function removeErrorHandler(handler: (error: AppError) => void): void;
+
 
   // './parcels/mount-parcel.js'
   export function mountRootParcel(parcelConfig: ParcelConfig, parcelProps: object): Parcel;
