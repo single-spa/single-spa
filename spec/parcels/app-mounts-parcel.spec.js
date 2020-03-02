c6e7413064481ca356ed064b213fe59b7dcefccf
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-02 10:06:19 -0700

    WIP: adding another parcel test for multiple parcels  (#439)
    * adding another parcel test for multiple parcels https://single-spa.slack.com/archives/C8R6U7MT7/p1581607307317300
    
    * tests are working exactly right

--- spec/parcels/app-mounts-parcel.spec.js
+++ spec/parcels/app-mounts-parcel.spec.js
@@ -232,6 +232,68 @@ describe("applications mounting parcels :", () => {
         expect(app.unmountCalls).toBe(1);
       });
   });
+
+  it(`correctly unmounts multiple parcels`, () => {
+    let shouldAppBeMounted = false,
+      parcel,
+      parcel2;
+    singleSpa.registerApplication(
+      "multiple-parcels",
+      app,
+      () => shouldAppBeMounted
+    );
+    parcelConfig = createParcelConfig();
+    const parcelConfig2 = createParcelConfig();
+
+    shouldAppBeMounted = true;
+
+    return singleSpa
+      .triggerAppChange()
+      .then(() => {
+        parcel = app.mountProps.mountParcel(parcelConfig, {
+          domElement: document.createElement("div")
+        });
+        parcel2 = app.mountProps.mountParcel(parcelConfig2, {
+          domElement: document.createElement("div")
+        });
+        expect(parcelConfig.mountCalls).toBe(0);
+        expect(parcelConfig2.mountCalls).toBe(0);
+        return Promise.all([
+          parcel.mountPromise,
+          parcel2.mountPromise
+        ]).then(() => [parcel, parcel2]);
+      })
+      .then(([p, p2]) => {
+        expect(parcelConfig.mountCalls).toBe(1);
+        expect(parcelConfig2.mountCalls).toBe(1);
+        expect(p.getStatus()).toBe(singleSpa.MOUNTED);
+        expect(p2.getStatus()).toBe(singleSpa.MOUNTED);
+        return [p, p2];
+      })
+      .then(([p, p2]) => {
+        shouldAppBeMounted = false;
+        expect(parcelConfig.mountCalls).toBe(1);
+        expect(parcelConfig2.mountCalls).toBe(1);
+        expect(parcelConfig.unmountCalls).toBe(0);
+        expect(parcelConfig2.unmountCalls).toBe(0);
+        expect(p.getStatus()).toBe(singleSpa.MOUNTED);
+        expect(p2.getStatus()).toBe(singleSpa.MOUNTED);
+        return singleSpa
+          .triggerAppChange()
+          .then(() => {
+            return Promise.all([p.unmountPromise, p2.unmountPromise]);
+          })
+          .then(() => [p, p2]);
+      })
+      .then(([p, p2]) => {
+        expect(parcelConfig.mountCalls).toBe(1);
+        expect(parcelConfig2.mountCalls).toBe(1);
+        expect(parcelConfig.unmountCalls).toBe(1);
+        expect(parcelConfig2.unmountCalls).toBe(1);
+        expect(p.getStatus()).toBe(singleSpa.NOT_MOUNTED);
+        expect(p2.getStatus()).toBe(singleSpa.NOT_MOUNTED);
+      });
+  });
 });
 
 function createParcelConfig() {
