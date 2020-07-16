d06d5a45531f5cebf2d874329f32d071cc0cc5cd
Pierre <pooria.khodaveissi@gmail.com>
2020-07-16 19:54:04 +0430

    fix: toDymanicPathValidatorRegex-supports-queryParams-and-urlFragments (#600)
    * fix: toDymanicPathValidatorRegex-supports-queryParams-and-urlFragments
    
    * fix: updates on path-to-active-when specs to support url-fragments
    
    * chore: path-to-active-when-specs: adds url-fragment case to hash based routes
    
    * fix: toDynamicPathValidatorRegex: removes ? char from regexStr

--- spec/apis/path-to-active-when.spec.js
+++ spec/apis/path-to-active-when.spec.js
@@ -6,6 +6,7 @@ describe(`pathToActiveWhen`, () => {
     expectPathToMatch("/pathname", {
       "http://app.com/pathname": true,
       "http://app.com/pathname?query-string=1": true,
+      "http://app.com/pathname#url-fragment": true,
       "http://app.com/pathname/": true,
       "http://app.com/pathname/?query-string=1": true,
       "http://app.com/pathname/anything/everything": true,
@@ -17,6 +18,7 @@ describe(`pathToActiveWhen`, () => {
     expectPathToMatch("/pathname/", {
       "http://app.com/pathname/": true,
       "http://app.com/pathname/?query-string=1": true,
+      "http://app.com/pathname/#url-fragment": true,
       "http://app.com/pathname/extra": true,
       "http://app.com/pathname": false,
       "http://app.com/pathname?query-string=1": false,
@@ -37,6 +39,7 @@ describe(`pathToActiveWhen`, () => {
       "http://app.com/#/pathname/": true,
       "http://app.com/#/pathname/?query-string=1": true,
       "http://app.com/#/pathname/anything/everything": true,
+      "http://app.com/#/pathname/anything/everything#url-fragment": true,
       "http://app.com/#/pathname/anything/everything?query-string=1": true,
       "http://app.com?query-string=1/#/pathname/anything/everything?query-string=1": true,
       "http://app.com?query-string=1#/pathname/anything/everything?query-string=1": true,
