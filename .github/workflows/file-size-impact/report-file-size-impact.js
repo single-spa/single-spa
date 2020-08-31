4782fc82d8a4588776ce0b79993edd427949a06e
Tiago de Oliveira Stutz <tiagostutz@gmail.com>
2020-08-31 12:19:28 +0000

    fix: removed commands that replicates default behaviors

--- .github/workflows/file-size-impact/report-file-size-impact.js
+++ .github/workflows/file-size-impact/report-file-size-impact.js
@@ -1,10 +1,10 @@
-const { reportFileSizeImpact, readGithubWorkflowEnv } = require("@jsenv/file-size-impact");
+const {
+  reportFileSizeImpact,
+  readGithubWorkflowEnv,
+} = require("@jsenv/file-size-impact");
 
 reportFileSizeImpact({
   ...readGithubWorkflowEnv(),
-  projectDirectoryUrl: process.env.GITHUB_WORKSPACE,
-  installCommand: "npm install",
-  buildCommand: "npm run build && rm -rf node_modules",
   trackingConfig: {
     dist: {
       "./lib/**/*": true,
