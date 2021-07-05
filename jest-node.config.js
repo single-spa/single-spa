8cf52b91ef78d3913c5a56900574fe0a775c210f
Joel Denning <joeldenning@gmail.com>
2021-07-05 13:30:14 -0600

    Fix

--- jest-node.config.js
+++ jest-node.config.js
@@ -4,4 +4,8 @@ export default {
     "single-spa": "<rootDir>/src/single-spa.js",
   },
   testRegex: "node-spec/.+.(spec|test).js",
+  globals: {
+    __SINGLE_SPA_DEVTOOLS__: {},
+    __DEV__: true,
+  },
 };
