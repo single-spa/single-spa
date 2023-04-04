ce414dadc1db5847fd73b50d5b9da812a741c430
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-04-04 22:58:54 +0530

    add helper to retrieve dynamic path regex info

--- src/applications/app.helpers.js
+++ src/applications/app.helpers.js
@@ -38,3 +38,68 @@ export function isParcel(appOrParcel) {
 export function objectType(appOrParcel) {
   return isParcel(appOrParcel) ? "parcel" : "application";
 }
+
+export function toDynamicPathRegexInfo(path, exactMatch) {
+  let lastIndex = 0,
+    inDynamic = false,
+    regexStr = "^";
+
+  const paramNames = [];
+
+  if (path[0] !== "/") {
+    path = "/" + path;
+  }
+
+  for (let charIndex = 0; charIndex < path.length; charIndex++) {
+    const char = path[charIndex];
+    const startOfDynamic = !inDynamic && char === ":";
+    const endOfDynamic = inDynamic && char === "/";
+    if (startOfDynamic || endOfDynamic) {
+      appendToRegex(charIndex);
+    }
+    if (startOfDynamic) {
+      const paramName = path.slice(charIndex + 1, path.indexOf("/", charIndex));
+      paramNames.push(paramName);
+    }
+  }
+
+  appendToRegex(path.length);
+  return { regex: new RegExp(regexStr, "i"), paramNames };
+
+  function appendToRegex(index) {
+    const anyCharMaybeTrailingSlashRegex = "[^/]+/?";
+    const commonStringSubPath = escapeStrRegex(path.slice(lastIndex, index));
+
+    regexStr += inDynamic
+      ? anyCharMaybeTrailingSlashRegex
+      : commonStringSubPath;
+
+    if (index === path.length) {
+      if (inDynamic) {
+        if (exactMatch) {
+          // Ensure exact match paths that end in a dynamic portion don't match
+          // urls with characters after a slash after the dynamic portion.
+          regexStr += "$";
+        }
+      } else {
+        // For exact matches, expect no more characters. Otherwise, allow
+        // any characters.
+        const suffix = exactMatch ? "" : ".*";
+
+        regexStr =
+          // use charAt instead as we could not use es6 method endsWith
+          regexStr.charAt(regexStr.length - 1) === "/"
+            ? `${regexStr}${suffix}$`
+            : `${regexStr}(/${suffix})?(#.*)?$`;
+      }
+    }
+
+    inDynamic = !inDynamic;
+    lastIndex = index;
+  }
+
+  function escapeStrRegex(str) {
+    // borrowed from https://github.com/sindresorhus/escape-string-regexp/blob/master/index.js
+    return str.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&");
+  }
+}
