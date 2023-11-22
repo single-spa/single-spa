ccd4381f6321f4ab4187a368d7cad49bb602ff8f
joeldenning <joeldenning@gmail.com>
2023-11-21 21:01:17 -0700

    Implement unregisterApplication server-side

--- src/applications/apps.js
+++ src/applications/apps.js
@@ -169,8 +169,12 @@ export function unregisterApplication(appName) {
     );
   }
 
+  const unloadPromise = isInBrowser
+    ? unloadApplication(appName, { waitForUnmount: false })
+    : Promise.resolve();
+
   // See https://github.com/single-spa/single-spa/issues/871 for why waitForUnmount is false
-  return unloadApplication(appName, { waitForUnmount: false }).then(() => {
+  return unloadPromise.then(() => {
     const appIndex = apps.map(toName).indexOf(appName);
     apps.splice(appIndex, 1);
   });
