c4d099375078740af4ea3f96688f492b38cee969
Milan Kovacic <MilanKovacic@users.noreply.github.com>
2024-10-06 05:36:29 +0200

    Fix package exports (#1260)
    * fix: package exports
    
    * add changeset
    
    * remove typings folder from clean script, gitignore, and prettierignore
    
    * remove outDir, and emitDeclarationOnly from tsconfig
    
    * rename types-tsc to types-unbundled
    
    * replace remove-extraneous-types script with rimraf
    
    * move types to root of each format's output
    
    * change typescript target to ES2015
    
    * remove outDir from typescript plugin configuration
    
    * enforce typechecking in build
    
    ---------
    
    Co-authored-by: MilanKovacic <9189985+MilanKovacic@users.noreply.github.com>

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,9 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import analyzer from "rollup-plugin-analyzer";
 import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
 import packageJson from "./package.json" with { type: "json" };
+import { dts } from "rollup-plugin-dts";
 
 const isProduction = process.env.ROLLUP_ENV === "production";
 const useAnalyzer = process.env.ANALYZER === "analyzer";
@@ -37,44 +39,91 @@ const resolveOpts = {
   extensions: [".ts", ".js"],
 };
 
-export default (async () => [
-  {
-    input: `./src/single-spa${isProduction ? "" : ".profile"}.ts`,
-    output: [
-      {
-        file: `./lib/umd/single-spa${isProduction ? ".min" : ".dev"}.cjs`,
-        format: "umd",
-        name: "singleSpa",
-        sourcemap: true,
-        banner: generateBanner("UMD"),
-      },
-      {
-        file: `./lib/system/single-spa${isProduction ? ".min" : ".dev"}.js`,
-        format: "system",
-        sourcemap: true,
-        banner: generateBanner("SystemJS"),
-      },
-      {
-        file: `./lib/esm/single-spa${isProduction ? ".min" : ".dev"}.js`,
-        format: "esm",
-        sourcemap: true,
-        banner: generateBanner("ESM"),
-      },
-    ],
-    plugins: [
-      replace(replaceOpts),
-      resolve(resolveOpts),
-      babel(babelOpts),
-      commonjs(),
-      isProduction &&
-        (await import("@rollup/plugin-terser")).default(terserOpts),
-      useAnalyzer && analyzer(),
-    ],
-  },
-])();
-
 function generateBanner(format) {
   return `/* single-spa@${packageJson.version} - ${format} - ${
     isProduction ? "prod" : "dev"
   } */`;
 }
+
+const commonPlugins = [
+  replace(replaceOpts),
+  resolve(resolveOpts),
+  babel(babelOpts),
+  commonjs(),
+  isProduction && (await import("@rollup/plugin-terser")).default(terserOpts),
+  useAnalyzer && analyzer(),
+];
+
+const commonConfiguration = {
+  input: `./src/single-spa${isProduction ? "" : ".profile"}.ts`,
+};
+
+export default async () => [
+  // UMD build
+  {
+    ...commonConfiguration,
+    output: {
+      file: `./lib/umd/single-spa${isProduction ? ".min" : ".dev"}.cjs`,
+      format: "umd",
+      name: "singleSpa",
+      sourcemap: true,
+      banner: generateBanner("UMD"),
+    },
+    plugins: [
+      ...commonPlugins,
+      // Typecheck, and emit declaration files
+      typescript({
+        declarationDir: "./lib/umd/types-unbundled",
+        noEmitOnError: true,
+      }),
+    ],
+  },
+  // ESM build
+  {
+    ...commonConfiguration,
+    output: {
+      file: `./lib/esm/single-spa${isProduction ? ".min" : ".dev"}.js`,
+      format: "esm",
+      sourcemap: true,
+      banner: generateBanner("ESM"),
+    },
+    plugins: [
+      ...commonPlugins,
+      // Typecheck, and emit declaration files
+      typescript({
+        declarationDir: "./lib/esm/types-unbundled",
+        noEmitOnError: true,
+      }),
+    ],
+  },
+  // System build
+  {
+    ...commonConfiguration,
+    output: {
+      file: `./lib/system/single-spa${isProduction ? ".min" : ".dev"}.cjs`,
+      format: "system",
+      sourcemap: true,
+      banner: generateBanner("SystemJS"),
+    },
+    plugins: [...commonPlugins],
+  },
+  // For compatibility with node16 module resolution, types are bundled
+  // Bundle ESM types
+  {
+    input: `./lib/esm/types-unbundled/single-spa.d.ts`,
+    output: {
+      file: `./lib/esm/single-spa.d.ts`,
+      format: "es",
+    },
+    plugins: [dts()],
+  },
+  // Bundle CJS types
+  {
+    input: `./lib/umd/types-unbundled/single-spa.d.ts`,
+    output: {
+      file: `./lib/umd/single-spa.d.cts`,
+      format: "cjs",
+    },
+    plugins: [dts()],
+  },
+];
